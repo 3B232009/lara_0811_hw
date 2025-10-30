008c9f40e169a580203259dc8eb79b3d13ea3cd8
Huang <3b232009@gm.student.ncut.edu.tw>
2025-10-30 23:16:21 +0800

    完成關於本站路由與 view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>關於本站</title>
+</head>
+<body>
+    <h1>關於本站</h1>
+    <a href="/">首頁</a> |
+    <a href="/news">最新消息</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
